9cfa51ff1d75474ceb029a682a099e503b9f16a4
srsuper <aot5310@gmail.com>
2021-05-04 15:15:29 +0700

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -17,7 +17,7 @@ app.listen(app.get('port'));
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-var token = process.env.TOKEN || 'token';
+var token = process.env.TOKEN || 'PNCK';
 var received_updates = [];
 
 app.get('/', function(req, res) {
